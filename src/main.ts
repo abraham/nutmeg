1f66dc577c3c64ff2ea46e3de7d07ebd412542d0
Abraham Williams <4braham@gmail.com>
2017-10-10 14:40:10 -0500

    Version 0.1.10

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import program = require('commander');
 
 program
-  .version('0.1.9')
+  .version('0.1.10')
   .usage('new <name> [property:type...]')
   .command('new <name> [property:type...]', 'generate a Web Component');
 
