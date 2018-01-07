dbd2429a2daeca849c812d9b47da479845347e9f
Abraham Williams <4braham@gmail.com>
2018-01-07 12:04:45 -0600

    Move getter/setter to their own functions

--- src/decorators.ts
+++ src/decorators.ts
@@ -7,6 +7,48 @@ function shouldObserveProperty(target: HTMLElement, key: string, type: any) {
   return !primitiveTypes.includes(type) && !(<any>target).constructor['observedProperties'].includes(key);
 }
 
+function getter(key: string, type: any) {
+  return function(this: Element) {
+    switch(type) {
+      case String:
+        return this.getAttribute(key);
+      case Number:
+        if (this.hasAttribute(key)) {
+          return Number(this.getAttribute(key));
+        } else {
+          return null;
+        }
+      case Boolean:
+        return this.hasAttribute(key);
+      default:
+        return (<any>this)[`_${key}`];
+    }
+  }
+}
+
+function setter(key: string, type: any) {
+  return function(this: Element, value: any) {
+    if (value === null || value === undefined || value === false) {
+      this.removeAttribute(key);
+    } else {
+      switch (type) {
+        case String:
+          this.setAttribute(key, String(value));
+          break;
+        case Number:
+          this.setAttribute(key, String(value));
+          break;
+        case Boolean:
+          this.setAttribute(key, '');
+          break;
+        default:
+         (<any>this)[`_${key}`] = value;
+      }
+    }
+    this.render();
+  };
+}
+
 export function Property() {
   return function(target: HTMLElement, key: string) {
     const type = Reflect.getMetadata('design:type', target, key);
@@ -17,42 +59,8 @@ export function Property() {
     Object.defineProperty(target, key, {
       configurable: true,
       enumerable: true,
-      get(this: Element) {
-        switch(type) {
-          case String:
-            return this.getAttribute(key);
-          case Number:
-            if (this.hasAttribute(key)) {
-              return Number(this.getAttribute(key));
-            } else {
-              return null;
-            }
-          case Boolean:
-            return this.hasAttribute(key);
-          default:
-            return (<any>this)[`_${key}`];
-        }
-      },
-      set(this: Element, value) {
-        if (value === null || value === undefined || value === false) {
-          this.removeAttribute(key);
-        } else {
-          switch (type) {
-            case String:
-              this.setAttribute(key, String(value));
-              break;
-            case Number:
-              this.setAttribute(key, String(value));
-              break;
-            case Boolean:
-              this.setAttribute(key, '');
-              break;
-            default:
-             (<any>this)[`_${key}`] = value;
-          }
-        }
-        this.render();
-      },
+      get: getter(key, type),
+      set: setter(key, type),
     });
   }
 }
