7ffdf57fb56baf7579eb042e7eb7590b43a9d4f3
Abraham Williams <4braham@gmail.com>
2018-01-07 19:52:37 -0600

    Remove unused code

--- src/test.ts
+++ src/test.ts
@@ -9,18 +9,9 @@ program.command('test [path]', 'test a Web Component')
 
 const nutmegDir = path.resolve(process.argv[1], '../..');
 const workingDir = path.resolve(program.args[0]);
-const distDir = path.resolve(workingDir, './dist');
-const typescriptConfigFile = path.resolve(workingDir, 'tsconfig.json');
-const webpackConfigFile = path.resolve(nutmegDir, 'webpack.component.config.js');
-const tag = workingDir.split('/')[workingDir.split('/').length - 1];
-const tscCmd = `tsc --project ${typescriptConfigFile} --watch`;
-const webpackCmd = `webpack --config ${webpackConfigFile} --env.tag=${tag} --env.workingDir=${workingDir} --watch`;
 
 const karmaConfigFile = path.resolve(nutmegDir, 'karma.component.config.js');
 const karmaCmd = `karma start ${karmaConfigFile}`;
-
-console.log('nutmegDir', nutmegDir, 'workingDir', workingDir);
-console.log('karmaCmd', karmaCmd);
 
 exit("Directory doesn't have a package.json with @nutmeg/element as a dependancy.", !isNutmegComponent(workingDir));
 
