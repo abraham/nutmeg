519d0e1c9bc4b41700aca353b96fa98e8deab043
Abraham Williams <4braham@gmail.com>
2018-10-07 18:32:00 -0500

    Only pack CLI

--- packages/cli/scripts/test.ts
+++ packages/cli/scripts/test.ts
@@ -1,20 +1,16 @@
 import * as shell from 'shelljs';
 import * as path from 'path';
 import * as cliPkg from '../package.json';
-import * as seedPkg from '../../seed/package.json';
 
-const debug = process.env.DEBUG === 'true'
-shell.config.verbose = debug;
-const silent = !debug;
+shell.config.verbose = process.env.DEBUG === 'true';
+
 const cliTgzName = `nutmeg-cli-${cliPkg.version}.tgz`;
-const seedTgzName = `nutmeg-seed-${seedPkg.version}.tgz`;
 const pkgsDir = path.resolve('..');
 const cliDir = path.resolve(pkgsDir, 'cli');
 const seedDir = path.resolve(pkgsDir, 'seed');
 const cliTgz = path.resolve(cliDir, cliTgzName);
-const seedTgz = path.resolve(seedDir, seedTgzName);
 const nutmegPath = path.resolve(cliDir, 'bin', 'nutmeg');
-const sources = `--cli-source file:${cliTgz} --seed-source file:${seedTgz}`;
+const sources = `--cli-source file:${cliTgz} --seed-source file:${seedDir}`;
 const attributes = 'first:number second:string third:boolean';
 const installCmd = `${nutmegPath} new ci-test ${attributes} ${sources}`;
 const testDir = path.resolve(shell.tempdir(), 'nutmeg');
@@ -31,13 +27,11 @@ removeTestFiles();
 function setupForTests() {
   removeTestFiles();
   shell.mkdir(testDir);
-  [cliDir, seedDir].forEach(dir => {
-    shell.cd(dir);
-    shell.exec('npm pack .', { silent });
-  });
+  shell.cd(cliDir);
+  shell.exec('npm pack .', { silent: true });
 }
 
 function removeTestFiles() {
   shell.cd(cliDir);
-  shell.rm('-rf', [testDir, cliTgz, seedTgz]);
+  shell.rm('-rf', [testDir, cliTgz]);
 }
