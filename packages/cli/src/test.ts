e015781c6f104a5e822c18b3c837df39f0c65fa2
Abraham Williams <4braham@gmail.com>
2022-03-05 22:06:13 -0600

    Switch to Vitest

--- packages/cli/src/test.ts
+++ packages/cli/src/test.ts
@@ -10,13 +10,12 @@ const program = new Command();
 program.command('test <path>', 'test a Web Component').parse(process.argv);
 
 const workingDir = path.resolve(process.cwd(), program.args[0]);
-const karmaConfigFile = path.resolve(nutmegDir, 'karma.component.config.js');
-const karmaCmd = `karma start ${karmaConfigFile}`;
+const vitestCmd = `vitest --watch false --environment happy-dom`;
 
 exit(
   "Directory doesn't have a package.json with @nutmeg/seed as a dependancy.",
   !isNutmegComponent(workingDir)
 );
 
-const result = shell.exec(`npx ${karmaCmd}`);
+const result = shell.exec(`npx ${vitestCmd}`);
 process.exit(result.code);
