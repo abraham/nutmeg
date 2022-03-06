0ce48106c1966634be466a4aea0df489f9bd84c5
Abraham Williams <4braham@gmail.com>
2022-03-05 21:04:09 -0600

    Remove webpack

--- packages/cli/src/serve.ts
+++ packages/cli/src/serve.ts
@@ -1,14 +1,7 @@
 import { Command } from 'commander';
 import path from 'path';
 import shell from 'shelljs';
-import { Component } from './component';
-import {
-  exit,
-  isNutmegComponent,
-  notifyOfUpdate,
-  nutmegDir,
-  tsconfigPath,
-} from './utils';
+import { exit, isNutmegComponent, notifyOfUpdate, tsconfigPath } from './utils';
 
 notifyOfUpdate();
 
@@ -17,17 +10,11 @@ const program = new Command();
 program.command('build <path>', 'compile a Web Component').parse(process.argv);
 
 const workingDir = path.resolve(process.cwd(), program.args[0]);
-const webpackConfigFile = path.resolve(
-  nutmegDir,
-  'webpack.component.config.js'
-);
-const tag = Component.tagFromPackage(workingDir);
 const tscCmd = `tsc --project ${tsconfigPath(workingDir)} --watch`;
-const webpackCmd = `webpack-dev-server --config ${webpackConfigFile} --env.tag=${tag} --env.workingDir=${workingDir} --open`;
 
 exit(
   "Directory doesn't have a package.json with @nutmeg/seed as a dependancy.",
   !isNutmegComponent(workingDir)
 );
 
-shell.exec(`npx ${tscCmd} & npx ${webpackCmd}`);
+shell.exec(`npx ${tscCmd}`);
